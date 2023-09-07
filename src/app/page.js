8b509f8929983e613f69fad7b1a9ab2668fb4b76
Ayobami Lawal <bamiayo90@gmail.com>
2023-09-07 14:33:25 +0100

    next fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
+import Result from "@/components/Result";
 import Image from "next/image";
 
-export default function Home() {
+const api_key = process.env.API_KEY;
+const api_token = process.env.API_TOKEN;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  console.log(genre);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${api_token}`,
+    },
+    next: { revalidate: 10000 },
+  };
+  const url =
+    genre == "fetchTrending"
+      ? "https://api.themoviedb.org/3/trending/all/day?language=en-US"
+      : "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+  const response = await fetch(url, options);
+  const data = await response.json();
+  // console.log(data);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hi there
+      {/* <> */}
+      <Result />
     </main>
   );
 }
---
+++ src/components/Result.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Result({ result }) {
+  return <div>jjjj</div>;
+}
+
+export default Result;
